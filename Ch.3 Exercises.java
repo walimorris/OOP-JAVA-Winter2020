64a9c69ac1a31ef1edda9a2167e5d84e3accdb5c
walimorris <48896622+walimorris@users.noreply.github.com>
2020-01-11 23:30:44 -0800

    Update Ch.3 Exercises.java

--- Ch.3 Exercises.java
+++ Ch.3 Exercises.java
@@ -17,3 +17,24 @@ public class WriteSpaces {
         }
     }
 }
+
+/* Introductory Example: This program determines the amount of coffee a person
+ * drinks weekly; depending on the number of coffees consumed each day by the 
+ * number of days in a week.
+ *  
+ * Author : Wali Morris 
+ * File   : ReturnExample.java 
+ * Date   : 01/11/2020
+ */
+
+public class ReturnExample {
+    public static void main(String[] args) {
+        int numOfCoffeesADay = 3, numOfDays = 7;
+        int weeklyCoffee = coffeesAWeek(numOfCoffeesADay, numOfDays);
+        System.out.println("Number of coffees consumed this week: " + weeklyCoffee);
+    }
+
+    public static int coffeesAWeek(int x, int y) {
+        return x * y;
+    }
+}
