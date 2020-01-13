914c9f2cba2af3afebbb14389f15b80f92572b51
walimorris <48896622+walimorris@users.noreply.github.com>
2020-01-13 14:14:22 -0800

    Update Extra Exercises.java

--- Extra Exercises.java
+++ Extra Exercises.java
@@ -12,3 +12,21 @@ public class StringVariable {
         System.out.println(var.toUpperCase());
     }
 }
+
+/* From the command line this program allows you to print an arguement
+ * after Hello World is printed by using args in main. For instance, 
+ * after this program is compiled you can run java HelloWorldCL Wali
+ * and "Hello World Wali" will be printed in he console. I thought 
+ * this was pretty cool because I was always confused with what the
+ * reason for args has been. 
+ *
+ * Author : Wali Morris 
+ * File   : HelloWorldCL.java
+ * Date   : 01/13/2020
+ */
+
+public class HelloWorldCL {
+    public static void main(String[] args) {
+        System.out.println("Hello World " + args[0]);
+    }
+}
