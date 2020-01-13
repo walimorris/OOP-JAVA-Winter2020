7abda94e2cd6003404adaa26d768dd0caf7a1f82
walimorris <48896622+walimorris@users.noreply.github.com>
2020-01-13 03:54:02 -0800

    Create Extra Exercises.java

---
+++ Extra Exercises.java
@@ -0,0 +1,14 @@
+/* Write a Java program that uses a String variable to store a sentence and 
+ * then changes it to uppercase and displays it on the screen.
+ *
+ * Author : Wali Morris 
+ * File   : StringVariable.java
+ * Date   : 01/13/2020
+ */
+
+public class StringVariable {
+    public static void main(String[] args) {
+        String var = "Welcome to the new world!";
+        System.out.println(var.toUpperCase());
+    }
+}
