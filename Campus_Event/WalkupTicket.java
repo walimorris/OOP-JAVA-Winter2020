fed165d1cafd21cd6a4fc8898354e1a38ea026e1
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-03-14 00:35:58 -0700

    Update WalkupTicket.java

--- Campus_Event/WalkupTicket.java
+++ Campus_Event/WalkupTicket.java
@@ -1,10 +1,20 @@
+/**
+* Walkup Tickets are categorized as tickets brought at the event. 
+* 
+* @author Wali Morris 
+*/ 
+
 import java.text.*; 
 
 public class WalkupTicket extends Ticket { 
     public WalkupTicket() { 
 	super();
     }
-
+    
+    /**
+    * Walkup Tickets are set to a fixed price of 50 dollars. 
+    * @return price of walkup ticket
+    */
     public String getPrice() { 
 	return NumberFormat.getCurrencyInstance().format(50.00); 
     }
