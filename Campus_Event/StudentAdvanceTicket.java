780a291b55b1a5798c75e74d92f9370c2250bcde
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-03-14 00:53:43 -0700

    Update StudentAdvanceTicket.java

--- Campus_Event/StudentAdvanceTicket.java
+++ Campus_Event/StudentAdvanceTicket.java
@@ -1,10 +1,18 @@
+/**
+* Student Advance Tickets are the best priced when purchased in advance. For tickets
+* purchased 10 days or more in advance, the price is 15.00 dollars. Tickets purchased 
+* than 10 days in advance are 20.00 dollars. 
+* 
+* @author Wali Morris 
+*/
+
 import java.text.*; 
 
 public class StudentAdvanceTicket extends AdvanceTicket {
     private int days; 
 
     public StudentAdvanceTicket(int days) {
-	super(days);
+	super(days); // mainly called to generate random ticket number 
         this.days = days; 	
     } 
     
@@ -21,5 +29,4 @@ public class StudentAdvanceTicket extends AdvanceTicket {
 	return "Ticket#: " + this.getNumber() + ", " + "Price: " + this.getPrice() + 
 		" (ID Required)";
     } 
-}     
-    
+}    
